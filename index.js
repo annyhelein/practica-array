982da95b691919c4331849584ebfa46059a56921
sara rodriguez <sararbd25@gmail.com>
2024-03-05 22:10:53 -0500

    commit array CadenaDeTexto

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 let nada= [];
+
 let elementos = [1, 2, 3, 4, 5];
+
 let edtd = [1, "dos", true, { datos: "valor" }, [6, 7, 8]];
+
 let constructor = new Array(1, 2, 3, 4, 5);
+
 let secuenciaN = Array.from({ length: 5 }, (_, index) => index + 1);
+
 let valor = 0; // El valor que se repetirá
 let longitud = 5; // La longitud del array que se va a crear
 let array = new Array(longitud).fill(valor);
+
+let cadena = "Hola,  espero que te encuentres bien";
+let tex = string.split(", ");
