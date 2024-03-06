e9fad8012321dc32a1501dcada30cb8d11ceff5d
sara rodriguez <sararbd25@gmail.com>
2024-03-05 21:54:37 -0500

    array-commit-SecuenciaNumerica

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ let nada= [];
 let elementos = [1, 2, 3, 4, 5];
 let edtd = [1, "dos", true, { datos: "valor" }, [6, 7, 8]];
 let constructor = new Array(1, 2, 3, 4, 5);
+let secuenciaN = Array.from({ length: 5 }, (_, index) => index + 1);
